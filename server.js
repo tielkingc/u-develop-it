3bdf45a9767dee8380226e676e050a57539fc8c3
tielkingc <tielking5@gmail.com>
2021-06-22 16:24:45 -0400

    connected db to server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '12Wlkj77g!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello World'});
