7552e1ea6736e39017564f2a766bcebb00e9ff4a
tielkingc <tielking5@gmail.com>
2021-06-23 17:33:28 -0400

    added new routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const express = require('express');
+const { brotliDecompress } = require('zlib');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,7 +15,70 @@ const db = mysql.createConnection(
         database: 'election'
     },
     console.log('Connected to the election database.')
-)
+);
+
+app.get('/api/candidates', (req,res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+})
+
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id =?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello World'});
@@ -22,7 +86,7 @@ app.get('/', (req, res) => {
 
 app.use((req, res) => {
     res.status(404).end();
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
